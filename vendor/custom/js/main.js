293a0de61b4ac8d025b73c862c0bf281be9ba959
Allan Santos <allanzi.santos@gmail.com>
2018-04-10 22:20:40 -0300

    Implementado angular para index

--- vendor/custom/js/main.js
+++ vendor/custom/js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-    $('#image-detail').magnify();
-  });
+angular.module('astec', [])
+  .controller('IndexController', ['$http', '$scope', function ($http, $scope) {
+
+    $http.get('./../vendor/custom/json/index.json')
+      .then(function (response) {
+        $scope.products = response.data.products;
+        $scope.imagens = response.data.imagens;        
+      });
+  }]);
