ddb182de1a61d65709951d98322a7cb907f22174
Allan Santos <allanzi.santos@gmail.com>
2018-04-11 15:00:26 -0300

    Corrigido caminho para json

--- vendor/custom/js/main.js
+++ vendor/custom/js/main.js
@@ -1,7 +1,7 @@
 angular.module('astec', [])
   .controller('IndexController', ['$http', '$scope', function ($http, $scope) {
 
-    $http.get('./../vendor/custom/json/index.json')
+    $http.get('./vendor/custom/json/index.json')
       .then(function (response) {
         $scope.products = response.data.products;
         $scope.imagens = response.data.imagens;        
